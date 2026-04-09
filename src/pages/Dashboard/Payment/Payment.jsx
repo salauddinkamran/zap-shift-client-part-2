c016e456d254e346f2d227d3f92ea11fd9dcd554
salauddin kamran <kamransalauddin186@gmail.com>
2026-04-09 23:09:36 +0600

    payment stripe some update

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,20 +2,26 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router";
 import useAxiosSecure from "../../../hooks/useAxiosSecure/useAxiosSecure";
+import Loading from "../../../Components/Loading/Loading";
 
 const Payment = () => {
   const { parcelId } = useParams();
   const axiosSecure = useAxiosSecure();
   const { data: parcel, isLoading } = useQuery({
-    queryKey: ["parcels", parcelsId],
+    queryKey: ["parcels", parcelId],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels/${parcelId}`);
       return res.data;
     },
   });
+  console.log(parcel)
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2>Please Pay</h2>
+      <h2>Please Pay for : {parcel?.parcelName}</h2>
+      <button className="btn btn-primary text-black">Pay</button>
     </div>
   );
 };
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -73,7 +73,7 @@ export const router = createBrowserRouter([
         Component: MyParcels,
       },
       {
-        path: "payment/:parcleId",
+        path: "payment/:parcelId",
         Component: Payment,
       },
     ],
